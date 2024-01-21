86cfb1ef764de4fa51701927d69d3e409598dffd
shigexei <ezpz04@gmail.com>
2024-01-21 21:06:23 +0600

    if this works im gonna scream

---
+++ string_1.py
@@ -0,0 +1,24 @@
+x = "Hello world"
+print(x)
+
+txt = "Hello World"
+y = txt[0]
+print(y)
+
+z = txt[2:5]
+print(z)
+
+txt1 = " Hello World "
+a = txt1.strip()
+print(a)
+
+txt2 = "Hello World"
+txt2 = txt2.upper()
+print(txt2)
+
+txt3 = "Hello wOrlD"
+txt3 = txt3.lower()
+print(txt3)
+
+txt = "Hello World"
+txt = txt.replace("H", "J")
